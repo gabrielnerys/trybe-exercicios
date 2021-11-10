8f41f96d9c193e16dbc55ff54d2faa313a9c4bcc
gabenerys <gabe.nery@live.com>
2021-11-10 10:47:34 -0300

    Adiciona pasta aula 4-4 e script.js

---
+++ Fundamentos/bloco-04-Introdução-javascript-logica-de-programacao/aula-4-4/exercicios-fixação/script.js
@@ -0,0 +1,55 @@
+// let diasDaSemana = {
+//     1: 'domingo',
+//     2: 'segunda',
+//     3: 'terça',
+//     4: 'quarta',
+//     5: 'quinta',
+//     6: 'sexta',
+//     7: 'sábado',
+//     };
+    
+// console.log (diasDaSemana['6']);
+  
+// let moradores = [
+//     {
+//         nome: "Philipi",
+//         sobrenome: "Roberto",
+//         idade: "26",
+//         numAp: "102",
+//     },
+//     {
+//         nome: "Pedro",
+//         sobrenome: "Santos",
+//         idade: "22",
+//         numAp: "506",
+//     },
+//     {
+//         nome: "Isabela",
+//         sobrenome: "Ramos",
+//         idade: "27",
+//         numAp: "204",
+//     },
+//     {
+//         nome: "Ana",
+//         sobrenome: "Veiga",
+//         idade: "24",
+//         numAp: "305",
+//     }
+// ]
+// let primeiroMorador = moradores[2]
+// console.log ("A senhorita " + primeiroMorador.nome + ' ' + primeiroMorador.sobrenome + ', atual residente do apartamento de numero ' + primeiroMorador.numAp + ' é uma ótima moradora!');
+
+// let player = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: { golden: 2, silver: 3 }
+// }
+
+// console.log ('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.')
+
+// // Para incluir novos elementos ao final de um objeto ja criado, primeiro definir o nome do objeto, seguido pelo nome da chave como string entre colchetes e os dados em seguida.
+// player["bestInTheWorld"] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+// console.log ('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por 6 vezes seguidas nos anos de ' + player.bestInTheWorld + '.')
+// console.log ('A jogadora ' + player.name + ' possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.')
