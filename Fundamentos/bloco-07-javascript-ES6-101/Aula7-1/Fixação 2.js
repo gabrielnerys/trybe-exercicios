6fb47b6365390ab88f8a12f9d923a2af8bfb5bb8
gabenerys <gabe.nery@live.com>
2022-01-09 21:52:54 -0300

    Exercicio 4

--- Fundamentos/bloco-07-javascript-ES6-101/Aula7-1/Fixação 2.js
+++ Fundamentos/bloco-07-javascript-ES6-101/Aula7-1/Fixação 2.js
@@ -1,11 +1,11 @@
 // Crie uma função que receba um número e retorne seu fatorial.
 
 const factorial = (number) => {
-    
+
     let result = 1;
 
-    for (let i = 1; i <= number; i+=1)
-    result *= i
+    for (let i = 1; i <= number; i += 1)
+        result *= i
 
     return result
 }
@@ -17,3 +17,51 @@ console.log(factorial());
 const factorial2 = number => number > 1 ? number * factorial2(number - 1) : 1;
 console.log(factorial2(5));
 
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+const wordLength = text => {
+    let wordArray = text.split(' ');
+    let count = 0;
+    let result = '';
+
+    for (let word of wordArray) {
+        if (word.length > count) {
+            count = word.length;
+            result = word
+        }
+    }
+    return result;
+}
+
+console.log(wordLength('Então faz mto tempo que eu vi a primeira temporada'))
+
+// Exercicio 4
+
+const array = ["Android", "iOS", "Architecture", "Teach", "Run"];
+
+function buildSkillsPhrase(paramOne) {
+    const fun1 = paramInner => (
+        `Tryber ${paramInner} aqui!
+
+      Tudo bem?`
+    );
+
+    let result = `${fun1(paramOne)}
+
+    Minhas cinco principais habilidades são:`;
+
+    array.forEach((skill) =>
+        result = `${result}
+
+    - ${skill}`);
+
+    result = `
+    ${result}
+
+    #goTrybe
+    `;
+
+    return result;
+}
+
+console.log(buildSkillsPhrase("Lucas"));
