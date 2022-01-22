dae5406ee63e778129633beae2f7b8bbcb5db392
gabenerys <gabe.nery@live.com>
2022-01-22 09:56:54 -0300

    Exercicio de fixaçao 1 Aula 10.1

--- Módulo 2 - Front-end/Bloco 10 - React/Aula 10.1/hello-react/src/App.js
+++ Módulo 2 - Front-end/Bloco 10 - React/Aula 10.1/hello-react/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Component } from 'react/cjs/react.production.min';
 import './App.css';
-import Title from './Title';
+// import Title from './Title';
 
-function App() {
-  return <Title />
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
 }
+
+const lista = ['Acordar', 'Tomar café', 'Exercitar', 'Ler um livro', 'Meditar'];
+
+class App extends Component {
+  render(){
+    return (
+      <ul>{ lista.map(lista => Task(lista)) }</ul>
+    );
+  }
+}
+
+
+// function App() {
+//   return <Title />
+// }
 
 export default App;
