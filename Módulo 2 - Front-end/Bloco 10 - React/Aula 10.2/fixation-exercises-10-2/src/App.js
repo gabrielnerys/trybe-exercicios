95b406571c8859e55f8af862cd6124ea2888d7a6
gabenerys <gabe.nery@live.com>
2022-01-24 15:46:28 -0300

    Exercicios de fixaçao Props

---
+++ Módulo 2 - Front-end/Bloco 10 - React/Aula 10.2/fixation-exercises-10-2/src/App.js
@@ -0,0 +1,14 @@
+import Image from './Image';
+import './App.css';
+import staringCat from './staringCat.jpg'
+
+function App() {
+  return (
+    <main>
+      <Image source={staringCat} alternativeText='Cute cat staring' />
+      {/* <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' /> */}
+    </main>
+  );
+}
+
+export default App;
