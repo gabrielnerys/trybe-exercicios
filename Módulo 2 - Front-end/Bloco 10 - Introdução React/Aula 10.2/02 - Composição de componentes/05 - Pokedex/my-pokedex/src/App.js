537489a2be6ed4f8502f82f1651f1e52fa548f7a
gabenerys <gabe.nery@live.com>
2022-01-29 18:03:29 -0300

    Adiciona exercicios aula 10.2

---
+++ Módulo 2 - Front-end/Bloco 10 - Introdução React/Aula 10.2/02 - Composição de componentes/05 - Pokedex/my-pokedex/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import pokemons from './Data'
+import Pokedex from './Pokedex';
+import './App.css'
+
+class App extends Component{
+  render(){
+    return(
+      <div className='App'>
+        <h1> Pokedex </h1>
+        <Pokedex pokemons={ pokemons } />
+      </div>
+    );
+  }
+}
+
+export default App;
