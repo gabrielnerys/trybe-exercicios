4eebe629ae7382332c69a2c41c46565d15d05846
gabenerys <gabe.nery@live.com>
2022-02-17 08:39:20 -0300

    Atualiza Readme

--- Módulo 2 - Front-end/Bloco 14 - React Testing Library/Aula 1/Fixação 1/testes-react/src/App.js
+++ Módulo 2 - Front-end/Bloco 14 - React Testing Library/Aula 1/Fixação 1/testes-react/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Algo que não aparece
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <label htmlFor="id-email">
+          Email
+          <input id="id-email" type="email" />
+        </label>
+        <input id="btn-send" type="button" data-testid="id-send" value="Enviar"/>
+        <input id="btn-send" type="button" value="Voltar"/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- Módulo 2 - Front-end/Bloco 14 - React Testing Library/Aula 1/Fixação 1/testes-react/src/App.test.js
+++ Módulo 2 - Front-end/Bloco 14 - React Testing Library/Aula 1/Fixação 1/testes-react/src/App.test.js
@@ -3,6 +3,23 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/algo que não aparece/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputLabel = screen.getByLabelText(/email/i);
+  expect(inputLabel).toBeInTheDocument();
+  expect(inputLabel).toHaveProperty('type', 'email');
 });
+
+test('Verifica se existe um botão', () => {
+  render(<App />);
+  const button = screen.getAllByRole('button');
+  expect(button).toHaveLength(2);
+})
+
+test('Verifica se existe um botão com o data-testid="id-send"', () => {
+  render(<App />);
+  const buttonId = screen.getByTestId('id-send');
+  expect(buttonId).toBeInTheDocument();
+  expect(buttonId).toHaveProperty('type', 'button');
+  expect(buttonId).toHaveValue('Enviar');
+})
+
+test('Veri')
